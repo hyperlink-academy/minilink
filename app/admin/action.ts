3010d41b61459d2efccfdd032e2644c6e436028a
Jared Pereira <jared@awarm.space>
2024-09-30 19:08:46 -0400

    batch migration

--- app/admin/action.ts
+++ app/admin/action.ts
@@ -28,53 +28,61 @@ export async function action() {
     }, [] as string[]);
     console.log(unique_roots.length);
     let i = 0;
-    for (let old_root of unique_roots) {
-      console.log(`updating entity ${i} with id ${old_root}`);
-      i++;
-      let [existing_entity] = await tx
-        .select({ set: entities.set })
-        .from(entities)
-        .where(eq(entities.id, old_root));
+    let batch_size = 100;
+    while (i < unique_roots.length - batch_size) {
+      console.log(
+        `Processing batch ${i / batch_size + 1} of ${Math.ceil(unique_roots.length / batch_size)}`,
+      );
+      let batch = unique_roots.slice(i, i + batch_size);
+      await Promise.all(
+        batch.map(async (old_root) => {
+          let [existing_entity] = await tx
+            .select({ set: entities.set })
+            .from(entities)
+            .where(eq(entities.id, old_root));
 
-      if (!existing_entity)
-        throw new Error("Expected to find entity: " + old_root);
-      let [rootpagefact] = await tx
-        .select({})
-        .from(facts)
-        .where(
-          and(eq(facts.entity, old_root), eq(facts.attribute, "root/page")),
-        );
-      if (rootpagefact) {
-        console.log("already has root page");
-        continue;
-      }
-      let new_root = v7();
-      let [entity] = await tx
-        .insert(entities)
-        // And add it to that permission set
-        .values({ set: existing_entity.set, id: new_root })
-        .returning();
-      await tx.insert(facts).values([
-        {
-          id: v7(),
-          entity: new_root,
-          attribute: "root/page",
-          data: sql`${{ type: "ordered-reference", value: old_root, position: "a0" }}`,
-        },
-      ]);
-      await tx
-        .update(facts)
-        .set({ entity: new_root })
-        .where(
-          and(
-            eq(facts.entity, old_root),
-            inArray(facts.attribute, Object.keys(ThemeAttributes)),
-          ),
-        );
-      await tx
-        .update(permission_tokens)
-        .set({ root_entity: new_root })
-        .where(eq(permission_tokens.root_entity, old_root));
+          if (!existing_entity)
+            throw new Error("Expected to find entity: " + old_root);
+          let [rootpagefact] = await tx
+            .select({})
+            .from(facts)
+            .where(
+              and(eq(facts.entity, old_root), eq(facts.attribute, "root/page")),
+            );
+          if (rootpagefact) {
+            console.log("already has root page");
+            return;
+          }
+          let new_root = v7();
+          let [entity] = await tx
+            .insert(entities)
+            // And add it to that permission set
+            .values({ set: existing_entity.set, id: new_root })
+            .returning();
+          await tx.insert(facts).values([
+            {
+              id: v7(),
+              entity: new_root,
+              attribute: "root/page",
+              data: sql`${{ type: "ordered-reference", value: old_root, position: "a0" }}`,
+            },
+          ]);
+          await tx
+            .update(facts)
+            .set({ entity: new_root })
+            .where(
+              and(
+                eq(facts.entity, old_root),
+                inArray(facts.attribute, Object.keys(ThemeAttributes)),
+              ),
+            );
+          await tx
+            .update(permission_tokens)
+            .set({ root_entity: new_root })
+            .where(eq(permission_tokens.root_entity, old_root));
+        }),
+      );
+      i = i + batch_size;
     }
   });
   console.log("done");
